a5327a9edbfc9d4a5411a9c170ac97f55a46153a
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-06-08 11:47:44 +0200

    Basic items list

--- AspNetMvcProjekt/Controllers/CategoryController.cs
+++ AspNetMvcProjekt/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetMvcProjekt.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
 
@@ -7,12 +8,18 @@ public class CategoryController : Controller
     [Route("/")]
     public IActionResult All()
     {
-        return View();
+        return View(GetMockItems());
     }
 
     public IActionResult Category(string name)
     {
         ViewBag.CategoryName = name;
-        return View();
+        return View(GetMockItems());
     }
+
+    private List<Item> GetMockItems() => [
+        new() { Name = "Resistor 1k", Description = "1k E3", AmountInStorage = 2200, Price = 0.02M },
+        new() { Name = "Resistor 2k2", Description = "2k2 E3", AmountInStorage = 1200, Price = 0.02M },
+        new() { Name = "Resistor 4k7", Description = "4k7 E3", AmountInStorage = 200, Price = 0.02M },
+    ];
 }
