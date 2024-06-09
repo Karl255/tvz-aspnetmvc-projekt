bb9e96293c6141738c6e08b57a94a7cf47c01a3a
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-06-09 15:15:22 +0200

    Order management

---
+++ AspNetMvcProjekt/Controllers/OrderManagementController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetMvcProjekt.DAL;
+using AspNetMvcProjekt.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMvcProjekt.Web.Controllers;
+
+[Route("manage/orders")]
+public class OrderManagementController(
+	UserManager<User> userManager,
+	StoreDbContext dbContext
+) : BaseController(userManager)
+{
+	public IActionResult Index()
+	{
+		var orders = dbContext.Orders.Where(o => o.Status != OrderStatus.Cart).Include(o => o.User).ToList();
+		return View(orders);
+	}
+
+	[Route("{id}")]
+	public IActionResult Ship(int id)
+	{
+		var order = dbContext.Orders.FirstOrDefault(o => o.Id == id && o.Status == OrderStatus.Ordered);
+
+		if (order == null)
+		{
+			return NotFound();
+		}
+
+		order.Status = OrderStatus.Shipped;
+		dbContext.SaveChanges();
+
+		return RedirectToAction(nameof(Index));
+	}
+}
--- Model/User.cs
+++ Model/User.cs
@@ -6,6 +6,7 @@ public class User : IdentityUser<int>
 {
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string FullName => $"{Name} {Surname}";
 
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
